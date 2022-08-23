caeaba3e9fcbf0b70664f4becbf21a928fe5c2e0
Sharath <sharathkrml@gmail.com>
2022-08-23 14:25:52 +0530

    added entity to save events

--- rarible/src/exchange-v-1.ts
+++ rarible/src/exchange-v-1.ts
@@ -1,12 +1,39 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 import { Buy } from "../generated/ExchangeV1/ExchangeV1";
 import * as airstack from "./modules/airstack";
+import { Transaction } from "../generated/schema";
 export function handleBuy(event: Buy): void {
   if (
     event.params.buyTokenId != BigInt.fromI32(0) &&
     event.params.sellTokenId == BigInt.fromI32(0)
   ) {
     // BID
+    let sellAmount: BigInt;
+    if (event.params.amount == BigInt.fromI32(0)) {
+      sellAmount = event.params.amount;
+    } else {
+      sellAmount = event.params.buyValue
+        .times(event.params.amount)
+        .div(event.params.sellValue);
+      let transaction = Transaction.load(event.transaction.hash.toHexString());
+      if (!transaction) {
+        transaction = new Transaction(event.transaction.hash.toHexString());
+      }
+      transaction.type = "BID";
+      transaction.sellTokenId = event.params.sellTokenId;
+      transaction.sellValue = event.params.sellValue;
+      transaction.owner = event.params.owner;
+      transaction.buyToken = event.params.buyToken;
+      transaction.buyTokenId = event.params.buyTokenId;
+      transaction.buyValue = event.params.buyValue;
+      transaction.buyer = event.params.buyer;
+      transaction.amount = event.params.amount;
+      transaction.salt = event.params.salt;
+      transaction.sellAmount = sellAmount;
+      transaction.buyAmount = event.params.amount;
+      transaction.save();
+    }
+
     airstack.nft.trackNFTSaleTransactions(
       event.transaction.hash.toHexString(),
       [event.params.buyer], //from
@@ -14,11 +41,36 @@ export function handleBuy(event: Buy): void {
       [event.params.buyToken], //nft address
       [event.params.buyTokenId], // nft id
       event.params.sellToken, // token address
-      event.params.sellTokenId, // token price                      TODO: CHECK IT
+      sellAmount, // token amount                      TODO: CHECK IT
       event.block.timestamp
     );
   } else {
     // ORDER
+    let buyAmount: BigInt;
+    if (event.params.amount == BigInt.fromI32(0)) {
+      buyAmount = event.params.amount;
+    } else {
+      buyAmount = event.params.buyValue
+        .times(event.params.amount)
+        .div(event.params.sellValue);
+      let transaction = Transaction.load(event.transaction.hash.toHexString());
+      if (!transaction) {
+        transaction = new Transaction(event.transaction.hash.toHexString());
+      }
+      transaction.type = "ORDER";
+      transaction.sellTokenId = event.params.sellTokenId;
+      transaction.sellValue = event.params.sellValue;
+      transaction.owner = event.params.owner;
+      transaction.buyToken = event.params.buyToken;
+      transaction.buyTokenId = event.params.buyTokenId;
+      transaction.buyValue = event.params.buyValue;
+      transaction.buyer = event.params.buyer;
+      transaction.amount = event.params.amount; // amount of token bought
+      transaction.salt = event.params.salt;
+      transaction.sellAmount = event.params.amount;
+      transaction.buyAmount = event.params.amount;
+      transaction.save();
+    }
     airstack.nft.trackNFTSaleTransactions(
       event.transaction.hash.toHexString(),
       [event.params.owner], //from
@@ -26,7 +78,7 @@ export function handleBuy(event: Buy): void {
       [event.params.sellToken], //nft address
       [event.params.sellTokenId], // nft id
       event.params.buyToken, // token address
-      event.params.buyValue, // token price                         TODO: CHECK IT
+      event.params.buyValue, // token amount                         TODO: CHECK IT
       event.block.timestamp
     );
   }
