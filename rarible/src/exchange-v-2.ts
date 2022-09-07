3361178e2d8d85c94d72d44a3c4aff82f00cea94
Sharath V <sharathkrml@gmail.com>
2022-09-07 15:41:05 +0530

    completed V2 logic

--- rarible/src/exchange-v-2.ts
+++ rarible/src/exchange-v-2.ts
@@ -1,10 +1,11 @@
-import { Match as MatchEvent } from "../generated/ExchangeV2/ExchangeV2";
+import { MatchOrdersCall } from "../generated/ExchangeV2/ExchangeV2";
 import { Transaction } from "../generated/schema";
 import {
   BigInt,
   ByteArray,
   Bytes,
   ethereum,
+  log,
   TypedMap,
 } from "@graphprotocol/graph-ts";
 import {
@@ -18,110 +19,114 @@ import {
   SPECIAL,
   Asset,
   getNFTType,
+  getExchange,
+  calculatedTotal,
 } from "./rarible-helper";
 import * as airstack from "./modules/airstack";
 
-export function handleMatch(event: MatchEvent): void {
+export function handleMatchOrders(call: MatchOrdersCall): void {
+  let orderLeft = call.inputs.orderLeft;
+  let orderRight = call.inputs.orderRight;
+  let leftAssetType = getClass(orderLeft.makeAsset.assetType.assetClass);
+  let rightAssetType = getClass(orderRight.makeAsset.assetType.assetClass);
   let leftAsset = decodeAsset(
-    event.params.leftAsset.data,
-    event.params.leftAsset.assetClass
+    orderLeft.makeAsset.assetType.data,
+    leftAssetType
   );
   let rightAsset = decodeAsset(
-    event.params.rightAsset.data,
-    event.params.rightAsset.assetClass
+    orderRight.makeAsset.assetType.data,
+    rightAssetType
   );
-
-  if (leftAsset.assetClass == ERC20 || leftAsset.assetClass == ETH) {
-    // right is NFT
-    let tx = getOrCreateTransaction(event.transaction.hash, rightAsset);
-
-    tx.hash = event.transaction.hash.toHexString();
+  if (leftAssetType == ETH || leftAssetType == ERC20) {
+    let tx = getOrCreateTransaction(
+      call.transaction.hash,
+      rightAsset.address,
+      rightAsset.id
+    );
+    tx.hash = call.transaction.hash.toHexString();
+    tx.from = orderRight.maker.toHexString();
+    tx.to = orderLeft.maker.toHexString();
     tx.nft = "RIGHT";
-    tx.from = event.params.rightMaker.toHexString();
-    tx.to = event.params.leftMaker.toHexString();
     tx.nftAddress = rightAsset.address.toHexString();
     tx.nftId = rightAsset.id;
     tx.paymentToken = leftAsset.address.toHexString();
-    // tx.paymentAmount = event.params.newRightFill;
-    tx.paymentAmount = event.params.newRightFill.plus(
-      event.params.newRightFill
-        .times(BigInt.fromI32(250))
-        .div(BigInt.fromI32(10000))
+    let paymentAmount = calculatedTotal(
+      orderLeft.makeAsset.value,
+      orderLeft.dataType,
+      orderLeft.data
     );
-    tx.nftType = rightAsset.assetClass;
-
-    tx.blockHeight = event.block.number;
+    tx.paymentAmount = paymentAmount;
+    tx.blockHeight = call.block.number;
+    tx.exchange = getExchange(orderLeft.dataType);
     tx.save();
-
     airstack.nft.trackNFTSaleTransactions(
-      event.transaction.hash.toHexString(),
-      [event.params.rightMaker], //from
-      [event.params.leftMaker], //to
+      call.transaction.hash.toHexString(),
+      [orderRight.maker], //from
+      [orderLeft.maker], //to
       [rightAsset.address], //nft address
       [rightAsset.id], // nft id
       leftAsset.address, // token address
-      event.params.newRightFill.plus(
-        event.params.newRightFill
-          .times(BigInt.fromI32(250))
-          .div(BigInt.fromI32(10000))
-      ), // token amount
-      event.block.timestamp,
-      event.block.number
+      paymentAmount, // token amount
+      call.block.timestamp,
+      call.block.number
     );
   } else {
-    // left is NFT
-    let tx = getOrCreateTransaction(event.transaction.hash, leftAsset);
+    let tx = getOrCreateTransaction(
+      call.transaction.hash,
+      leftAsset.address,
+      leftAsset.id
+    );
+    tx.hash = call.transaction.hash.toHexString();
+    tx.from = orderLeft.maker.toHexString();
+    tx.to = orderRight.maker.toHexString();
     tx.nft = "LEFT";
-    tx.hash = event.transaction.hash.toHexString();
-    tx.from = event.params.leftMaker.toHexString();
-    tx.to = event.params.rightMaker.toHexString();
     tx.nftAddress = leftAsset.address.toHexString();
     tx.nftId = leftAsset.id;
     tx.paymentToken = rightAsset.address.toHexString();
-    tx.paymentAmount = event.params.newLeftFill.plus(
-      event.params.newLeftFill
-        .times(BigInt.fromI32(250))
-        .div(BigInt.fromI32(10000))
+    let paymentAmount = calculatedTotal(
+      orderRight.makeAsset.value,
+      orderRight.dataType,
+      orderRight.data
     );
-    tx.nftType = leftAsset.assetClass;
-    tx.blockHeight = event.block.number;
-    tx.save();
+    tx.paymentAmount = paymentAmount;
 
+    tx.exchange = getExchange(orderRight.dataType);
+    tx.blockHeight = call.block.number;
+    tx.save();
     airstack.nft.trackNFTSaleTransactions(
-      event.transaction.hash.toHexString(),
-      [event.params.leftMaker], //from
-      [event.params.rightMaker], //to
+      call.transaction.hash.toHexString(),
+      [orderLeft.maker], //from
+      [orderRight.maker], //to
       [leftAsset.address], //nft address
       [leftAsset.id], // nft id
       rightAsset.address, // token address
-      event.params.newLeftFill.plus(
-        event.params.newLeftFill
-          .times(BigInt.fromI32(250))
-          .div(BigInt.fromI32(10000))
-      ), // token amount
-      event.block.timestamp,
-      event.block.number
+      paymentAmount, // token amount
+      call.block.timestamp,
+      call.block.number
     );
   }
 }
 
-export function getOrCreateTransaction(hash: Bytes, asset: Asset): Transaction {
-  let tx = Transaction.load(
+export function getOrCreateTransaction(
+  hash: Bytes,
+  nftAddress: Bytes,
+  nftId: BigInt
+): Transaction {
+  let entity = Transaction.load(
     hash.toHexString() +
       "-" +
-      asset.address.toHexString() +
+      nftAddress.toHexString() +
       "-" +
-      asset.id.toHexString()
+      nftId.toHexString()
   );
-  if (!tx) {
-    tx = new Transaction(
+  if (!entity) {
+    entity = new Transaction(
       hash.toHexString() +
         "-" +
-        asset.address.toHexString() +
+        nftAddress.toHexString() +
         "-" +
-        asset.id.toHexString()
+        nftId.toHexString()
     );
   }
-  tx.exchange = "v2";
-  return tx;
+  return entity;
 }
--- rarible/src/rarible-helper.ts
+++ rarible/src/rarible-helper.ts
@@ -5,7 +5,7 @@ import {
   ethereum,
   TypedMap,
 } from "@graphprotocol/graph-ts";
-import { ZERO_ADDRESS, BIGINT_ZERO } from "./modules/prices/common/constants";
+import { log } from "@graphprotocol/graph-ts";
 import { ERC1155 as ERC1155Metadata } from "../generated/ExchangeV2/ERC1155";
 import { ERC721MetaData } from "../generated/ExchangeV2/ERC721MetaData";
 export const ERC20 = "ERC20";
@@ -16,6 +16,11 @@ export const COLLECTION = "COLLECTION";
 export const CRYPTOPUNKS = "CRYPTOPUNKS";
 export const SPECIAL = "SPECIAL";
 
+export const ZERO_ADDRESS = Address.fromString(
+  "0x0000000000000000000000000000000000000000"
+);
+export const BIGINT_ZERO = BigInt.fromI32(0);
+
 export const classMap = new TypedMap<string, string>();
 classMap.set("0xaaaebeba", ETH);
 classMap.set("0x8ae85d84", ERC20);
@@ -23,6 +28,19 @@ classMap.set("0x73ad2146", ERC721);
 classMap.set("0x973bb640", ERC1155);
 classMap.set("0xf63c2825", COLLECTION);
 classMap.set("0x3e6b89d4", CRYPTOPUNKS);
+
+export const V1 = "0x4c234266";
+export const V2 = "0x23d235ef";
+
+export function getExchange(dataType: Bytes): string {
+  if (dataType.toHexString() == V1) {
+    return "V2V1";
+  } else if (dataType.toHexString() == V2) {
+    return "V2V2";
+  }
+  return "";
+}
+
 export function getClass(assetClass: Bytes): string {
   let res = classMap.get(assetClass.toHexString());
   if (res) {
@@ -40,11 +58,10 @@ export class Asset {
     this.assetClass = assetClass;
   }
 }
-export function decodeAsset(data: Bytes, assetClass: Bytes): Asset {
-  let type = getClass(assetClass);
+export function decodeAsset(data: Bytes, type: string): Asset {
   if (type == ERC20) {
     let decoded = ethereum.decode("(address)", data);
-    if (decoded) {
+    if (decoded != null) {
       let decodedTuple = decoded.toTuple();
       let asset = new Asset(
         decodedTuple[0].toAddress(),
@@ -55,7 +72,7 @@ export function decodeAsset(data: Bytes, assetClass: Bytes): Asset {
     }
   } else if (type == ERC721 || type == ERC1155) {
     let decoded = ethereum.decode("(address,uint256)", data);
-    if (decoded) {
+    if (decoded != null) {
       let decodedTuple = decoded.toTuple();
       let address = decodedTuple[0].toAddress();
       let id = decodedTuple[1].toBigInt();
@@ -64,7 +81,7 @@ export function decodeAsset(data: Bytes, assetClass: Bytes): Asset {
     }
   } else if (type == SPECIAL) {
     let decoded = ethereum.decode("(address,uint256,uint256)", data);
-    if (decoded) {
+    if (decoded != null) {
       let decodedTuple = decoded.toTuple();
       let address = decodedTuple[0].toAddress();
       let id = decodedTuple[2].toBigInt();
@@ -100,4 +117,83 @@ export function getNFTType(nftAddress: Address): string {
     }
   }
   return "";
+}
+
+export function getOriginFees(exchangeType: Bytes, data: Bytes): BigInt {
+  if (exchangeType.toHexString() == V1) {
+    if (
+      data
+        .toHexString()
+        .startsWith(
+          "0x000000000000000000000000000000000000000000000000000000000000004"
+        )
+    ) {
+      data = Bytes.fromHexString(
+        "0x0000000000000000000000000000000000000000000000000000000000000020" +
+          data.toHexString().slice(2)
+      );
+      log.error("weird case {}", [data.toHexString()]);
+      let decoded = ethereum.decode(
+        "((address,uint96)[],(address,uint96)[])",
+        data
+      );
+      if (!decoded) {
+        log.error("{} not decoded", [data.toHexString()]);
+      } else {
+        let dataV1 = decoded.toTuple();
+        let originFeeArray = dataV1[1].toArray();
+        let originFee = BIGINT_ZERO;
+        for (let i = 0; i < originFeeArray.length; i++) {
+          let originFeeItem = originFeeArray[i].toTuple();
+          originFee = originFee.plus(originFeeItem[1].toBigInt());
+        }
+        return originFee;
+      }
+    }
+    let decoded = ethereum.decode(
+      "((address,uint96)[],(address,uint96)[])",
+      data
+    );
+    if (!decoded) {
+      log.error("{} not decoded", [data.toHexString()]);
+    } else {
+      let dataV1 = decoded.toTuple();
+      let originFeeArray = dataV1[1].toArray();
+      let originFee = BIGINT_ZERO;
+      for (let i = 0; i < originFeeArray.length; i++) {
+        let originFeeItem = originFeeArray[i].toTuple();
+        originFee = originFee.plus(originFeeItem[1].toBigInt());
+      }
+      return originFee;
+    }
+  } else if (exchangeType.toHexString() == V2) {
+    let decoded = ethereum.decode(
+      "((address,uint96)[],(address,uint96)[],bool)",
+      data
+    );
+
+    if (!decoded) {
+      log.error("{} not decoded", [data.toHexString()]);
+    } else {
+      let dataV2 = decoded.toTuple();
+      let originFeeArray = dataV2[1].toArray();
+      let originFee = BIGINT_ZERO;
+      for (let i = 0; i < originFeeArray.length; i++) {
+        let originFeeItem = originFeeArray[i].toTuple();
+        originFee = originFee.plus(originFeeItem[1].toBigInt());
+      }
+      return originFee;
+    }
+  }
+  log.error("Not V1/V2 data={}", [data.toHexString()]);
+  return BIGINT_ZERO;
+}
+export function calculatedTotal(
+  amt: BigInt,
+  exchangeType: Bytes,
+  data: Bytes
+): BigInt {
+  let originFees = getOriginFees(exchangeType, data);
+  let calculatedFees = amt.times(originFees).div(BigInt.fromI32(10000));
+  return amt.plus(calculatedFees);
 }
