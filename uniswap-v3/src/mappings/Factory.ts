bae3738a9ff37b072673a28867fd17b31af0aba2
Deepesh KN <deepesh@plgworks.com>
2022-09-09 13:32:33 +0400

    Fixed decimal breaking issue

--- uniswap-v3/src/mappings/Factory.ts
+++ uniswap-v3/src/mappings/Factory.ts
@@ -5,7 +5,8 @@ import { PoolCreated } from "../../generated/Factory/Factory";
 import { dex } from "../../modules/airstack";
 import { updateAirMeta } from "../../modules/airstack/common";
 import { Pool as PoolTemplate } from "../../generated/templates";
-import { BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+// import { Temp } from "../../generated/schema";
 
 // Liquidity pool is created from the Factory contract.
 // Create a pool entity and start monitoring events from the newly deployed pool contract specified in the subgraph.yaml.
@@ -30,5 +31,20 @@ export function handlePoolCreated(event: PoolCreated): void {
 
   // Create and track the newly created pool contract based on the template specified in the subgraph.yaml file.
   PoolTemplate.create(event.params.pool);
+
+  // let temp = Temp.load("temp");
+  // if (temp === null) {
+  //   temp = new Temp("temp");
+  //   temp.isWhitelisted = BigInt.fromI32(1);
+  //   temp.save();
+
+  //   PoolTemplate.create(
+  //     Address.fromString("0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8")
+  //   );
+  //   PoolTemplate.create(
+  //     Address.fromString("0x655E25feD94ddb846601705ACE4349815E2A95D1")
+  //   );
+  // }
+
   updateAirMeta(event);
 }
