cb51af67cdba97a61216a18f00aaf3115f40a16f
Sharath V <sharathkrml@gmail.com>
2022-09-22 12:41:07 +0530

    removed Transaction entity from v2

--- rarible/src/exchange-v-2.ts
+++ rarible/src/exchange-v-2.ts
@@ -1,25 +1,9 @@
 import { MatchOrdersCall } from "../generated/ExchangeV2/ExchangeV2";
-import { Transaction } from "../generated/schema";
 import {
-  BigInt,
-  ByteArray,
-  Bytes,
-  ethereum,
-  log,
-  TypedMap,
-} from "@graphprotocol/graph-ts";
-import {
-  classMap,
   ETH,
   ERC20,
-  ERC721,
-  ERC1155,
   getClass,
   decodeAsset,
-  SPECIAL,
-  Asset,
-  getNFTType,
-  getExchange,
   calculatedTotal,
 } from "./rarible-helper";
 import * as airstack from "./modules/airstack";
@@ -38,27 +22,13 @@ export function handleMatchOrders(call: MatchOrdersCall): void {
     rightAssetType
   );
   if (leftAssetType == ETH || leftAssetType == ERC20) {
-    let tx = getOrCreateTransaction(
-      call.transaction.hash,
-      rightAsset.address,
-      rightAsset.id
-    );
-    tx.hash = call.transaction.hash.toHexString();
-    tx.from = orderRight.maker.toHexString();
-    tx.to = orderLeft.maker.toHexString();
-    tx.nft = "RIGHT";
-    tx.nftAddress = rightAsset.address.toHexString();
-    tx.nftId = rightAsset.id;
-    tx.paymentToken = leftAsset.address.toHexString();
+    // rightAsset is NFT
     let paymentAmount = calculatedTotal(
       orderLeft.makeAsset.value,
       orderLeft.dataType,
       orderLeft.data
     );
-    tx.paymentAmount = paymentAmount;
-    tx.blockHeight = call.block.number;
-    tx.exchange = getExchange(orderLeft.dataType);
-    tx.save();
+
     airstack.nft.trackNFTSaleTransactions(
       call.transaction.hash.toHexString(),
       [orderRight.maker], //from
@@ -71,28 +41,13 @@ export function handleMatchOrders(call: MatchOrdersCall): void {
       call.block.number
     );
   } else {
-    let tx = getOrCreateTransaction(
-      call.transaction.hash,
-      leftAsset.address,
-      leftAsset.id
-    );
-    tx.hash = call.transaction.hash.toHexString();
-    tx.from = orderLeft.maker.toHexString();
-    tx.to = orderRight.maker.toHexString();
-    tx.nft = "LEFT";
-    tx.nftAddress = leftAsset.address.toHexString();
-    tx.nftId = leftAsset.id;
-    tx.paymentToken = rightAsset.address.toHexString();
+    // leftAsset is NFT
     let paymentAmount = calculatedTotal(
       orderRight.makeAsset.value,
       orderRight.dataType,
       orderRight.data
     );
-    tx.paymentAmount = paymentAmount;
 
-    tx.exchange = getExchange(orderRight.dataType);
-    tx.blockHeight = call.block.number;
-    tx.save();
     airstack.nft.trackNFTSaleTransactions(
       call.transaction.hash.toHexString(),
       [orderLeft.maker], //from
@@ -105,28 +60,4 @@ export function handleMatchOrders(call: MatchOrdersCall): void {
       call.block.number
     );
   }
-}
-
-export function getOrCreateTransaction(
-  hash: Bytes,
-  nftAddress: Bytes,
-  nftId: BigInt
-): Transaction {
-  let entity = Transaction.load(
-    hash.toHexString() +
-      "-" +
-      nftAddress.toHexString() +
-      "-" +
-      nftId.toHexString()
-  );
-  if (!entity) {
-    entity = new Transaction(
-      hash.toHexString() +
-        "-" +
-        nftAddress.toHexString() +
-        "-" +
-        nftId.toHexString()
-    );
-  }
-  return entity;
 }
