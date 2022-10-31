6744582ab4078d3ffffcc906a09ddc8a6a0d4510
Rahul Gupta <rahul7668gupta@gmail.com>
2022-10-31 10:40:39 +0530

    added abis and modules, wip

--- rarible-subgraph/src/exchange-v-1.ts
+++ rarible-subgraph/src/exchange-v-1.ts
@@ -1,64 +1,46 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  ExchangeV1,
-  Buy,
-  Cancel,
-  OwnershipTransferred
-} from "../generated/ExchangeV1/ExchangeV1"
-import { ExampleEntity } from "../generated/schema"
+import { BigInt } from "@graphprotocol/graph-ts";
+import { ExchangeCall } from "../generated/ExchangeV1/ExchangeV1";
+import * as airstack from "./modules/airstack";
 
-export function handleBuy(event: Buy): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleExchange(call: ExchangeCall): void {
+  let sellAsset = call.inputs.order.key.sellAsset;
+  let buyAsset = call.inputs.order.key.buyAsset;
+  if (sellAsset.assetType > 1) {
+    // sellAsset is NFT,the owner has it & wants buy Asset
+    let paying = call.inputs.order.buying
+      .times(call.inputs.amount)
+      .div(call.inputs.order.selling);
+    let fee = paying.times(call.inputs.buyerFee).div(BigInt.fromI32(10000));
+    let paymentAmount = paying.plus(fee);
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    airstack.nft.trackNFTSaleTransactions(
+      call.transaction.hash.toHexString(),
+      [call.inputs.order.key.owner], //from
+      [call.from], //to
+      [sellAsset.token], //nft address
+      [sellAsset.tokenId], // nft id
+      buyAsset.token, // token address
+      paymentAmount, // token amount
+      call.block.timestamp,
+      call.block.number
+    );
+  } else {
+    // buyAsset is NFT,the owner wants NFT & give token/ETH
+    let fee = call.inputs.amount
+      .times(call.inputs.order.sellerFee)
+      .div(BigInt.fromI32(10000));
+    let paymentAmount = call.inputs.amount.plus(fee);
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    airstack.nft.trackNFTSaleTransactions(
+      call.transaction.hash.toHexString(),
+      [call.from], //from
+      [call.inputs.order.key.owner], //to
+      [buyAsset.token], //nft address
+      [buyAsset.tokenId], // nft id
+      sellAsset.token, // token address
+      paymentAmount, // token amount
+      call.block.timestamp,
+      call.block.number
+    );
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.sellToken = event.params.sellToken
-  entity.sellTokenId = event.params.sellTokenId
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.beneficiary(...)
-  // - contract.buyerFeeSigner(...)
-  // - contract.erc20TransferProxy(...)
-  // - contract.isOwner(...)
-  // - contract.ordersHolder(...)
-  // - contract.owner(...)
-  // - contract.prepareBuyerFeeMessage(...)
-  // - contract.prepareMessage(...)
-  // - contract.state(...)
-  // - contract.transferProxy(...)
-  // - contract.transferProxyForDeprecated(...)
 }
-
-export function handleCancel(event: Cancel): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
