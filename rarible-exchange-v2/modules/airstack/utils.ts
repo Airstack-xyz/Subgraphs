c7a87f3c830463a8e000dcd4f925860e4479c9e6
Rahul Gupta <rahul7668gupta@gmail.com>
2022-12-30 11:29:16 +0530

    updated rarible subgraphs

--- rarible-exchange-v2/modules/airstack/utils.ts
+++ rarible-exchange-v2/modules/airstack/utils.ts
@@ -8,5 +8,5 @@ export const AIR_META_ID = "AIR_META";
 export const BIGINT_ONE = BigInt.fromI32(1);
 export const SUBGRAPH_VERSION = "1";
 
-export const SUBGRAPH_NAME = "rarible-exchange-v2";
+export const SUBGRAPH_NAME = "Rarible Exchange V2";
 export const SUBGRAPH_SLUG = "rarible-exchange-v2";
