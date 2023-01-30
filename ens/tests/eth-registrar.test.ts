05d95fc3ede5eb60679b2fe7070fbd8e0a0f8019
Rahul Gupta <rahul7668gupta@gmail.com>
2023-01-31 02:01:19 +0530

    added resolver assertion in setname preimage tests

--- ens/tests/eth-registrar.test.ts
+++ ens/tests/eth-registrar.test.ts
@@ -10,6 +10,7 @@ import { handleNameRenewedByController, handleNameRegistered, handleNameRenewed,
 import { getHandleNameRenewedByControllerEvent, getHandleNameRegisteredEvent, getHandleNameRenewedEvent, getHandleNameRegisteredByControllerOldEvent, getHandleNameRegisteredByControllerEvent } from "./eth-registrar-utils"
 import { ETHEREUM_MAINNET_ID, ZERO_ADDRESS, byteArrayFromHex, uint256ToByteArray } from "../modules/airstack/domain-name/utils"
 import { BIGINT_ONE } from "../modules/airstack/common"
+import { AirDomain } from "../generated/schema"
 
 const rootNode: ByteArray = byteArrayFromHex("93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae");
 
@@ -81,6 +82,11 @@ describe("Unit tests for eth registrar handlers", () => {
     assert.fieldEquals("AirDomain", domainId, "lastBlock", blockId);
     assert.fieldEquals("AirDomain", domainId, "labelName", event.params.name);
     assert.fieldEquals("AirDomain", domainId, "name", event.params.name.concat(".eth"));
+    // ReverseRegistrar
+    let domain = AirDomain.load(domainId)!;
+    assert.fieldEquals("ReverseRegistrar", domain.name!, "id", event.params.name.concat(".eth"));
+    assert.fieldEquals("ReverseRegistrar", domain.name!, "domain", domainId);
+    assert.fieldEquals("ReverseRegistrar", domain.name!, "createdAt", blockId);
   })
 
   test("test handleNameRegisteredByController", () => {
@@ -96,6 +102,11 @@ describe("Unit tests for eth registrar handlers", () => {
     assert.fieldEquals("AirDomain", domainId, "lastBlock", blockId);
     assert.fieldEquals("AirDomain", domainId, "labelName", event.params.name);
     assert.fieldEquals("AirDomain", domainId, "name", event.params.name.concat(".eth"));
+    // ReverseRegistrar
+    let domain = AirDomain.load(domainId)!;
+    assert.fieldEquals("ReverseRegistrar", domain.name!, "id", event.params.name.concat(".eth"));
+    assert.fieldEquals("ReverseRegistrar", domain.name!, "domain", domainId);
+    assert.fieldEquals("ReverseRegistrar", domain.name!, "createdAt", blockId);
   })
 
   test("test handleNameRenewedByController", () => {
@@ -121,5 +132,10 @@ describe("Unit tests for eth registrar handlers", () => {
     assert.fieldEquals("AirNameRenewedTransaction", nameRenewedId, "paymentToken", ETHEREUM_MAINNET_ID.concat("-").concat(ZERO_ADDRESS));
     assert.fieldEquals("AirNameRenewedTransaction", nameRenewedId, "renewer", ETHEREUM_MAINNET_ID.concat("-").concat(event.transaction.from.toHexString()));
     assert.fieldEquals("AirNameRenewedTransaction", nameRenewedId, "expiryTimestamp", event.params.expires.toString());
+    // ReverseRegistrar
+    let domain = AirDomain.load(domainId)!;
+    assert.fieldEquals("ReverseRegistrar", domain.name!, "id", event.params.name.concat(".eth"));
+    assert.fieldEquals("ReverseRegistrar", domain.name!, "domain", domainId);
+    assert.fieldEquals("ReverseRegistrar", domain.name!, "createdAt", blockId);
   })
 })
