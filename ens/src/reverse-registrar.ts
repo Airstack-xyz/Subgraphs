c7e4de9d8db43c3a815e0e051fc977b90d24f74b
Rahul Gupta <rahul7668gupta@gmail.com>
2023-01-31 07:34:31 +0530

    minor schema update

--- ens/src/reverse-registrar.ts
+++ ens/src/reverse-registrar.ts
@@ -10,7 +10,7 @@ import { TOKEN_ADDRESS_ENS } from "./utils";
  * @param call SetNameCall from ReverseRegistrar contract
  */
 export function handleSetName(call: SetNameCall): void {
-  log.info("handleSetName: name {} reverse node {} txhash {}", [call.inputs.name, call.outputs.value0.toHexString(), call.transaction.hash.toHexString()]);
+  log.info("handleSetName: name {} txhash {}", [call.inputs.name, call.transaction.hash.toHexString()]);
   airstack.domain.trackSetPrimaryDomainTransaction(
     call.inputs.name,
     ETHEREUM_MAINNET_ID,
