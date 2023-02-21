1aac85e45b4f7c0cb9dabc2003eb0292d0a1d962
Rahul Gupta <rahul7668gupta@gmail.com>
2023-02-21 15:39:04 +0530

    bug fix and update for nft module

--- seaport-subgraph/modules/airstack/nft-marketplace/nft.ts
+++ seaport-subgraph/modules/airstack/nft-marketplace/nft.ts
@@ -6,12 +6,11 @@ import {
 
 import {
     AirNftTransaction,
-    AirToken,
     AirNftSaleRoyalty,
 } from "../../../generated/schema";
 
 import { AIR_NFT_SALE_ENTITY_ID } from "./utils";
-import { updateAirEntityCounter, getOrCreateAirBlock, getOrCreateAirAccount } from "../common";
+import { updateAirEntityCounter, getOrCreateAirBlock, getOrCreateAirAccount, getOrCreateAirToken } from "../common";
 
 export namespace nft {
     export function trackNFTSaleTransactions(
@@ -30,12 +29,12 @@ export namespace nft {
         }
 
         let block = getOrCreateAirBlock(chainID, blockHeight, blockHash, timestamp);
-
+        block.save();
         let transactionCount = NftSales.length;
         for (let i = 0; i < transactionCount; i++) {
             // Payment Token
             let paymentToken = getOrCreateAirToken(chainID, NftSales[i].paymentToken.toHexString());
-
+            paymentToken.save();
             // Account
             let buyerAccount = getOrCreateAirAccount(chainID, NftSales[i].buyer.toHexString(), block);
             let sellerAccount = getOrCreateAirAccount(chainID, NftSales[i].seller.toHexString(), block);
@@ -47,6 +46,7 @@ export namespace nft {
             let saleToken = getOrCreateAirToken(
                 chainID, NftSales[i].nft.collection.toHexString()
             );
+            saleToken.save();
 
             // Transaction
             let transactionId = getNFTSaleTransactionId(
@@ -127,16 +127,6 @@ export namespace nft {
             .concat(contractAddress)
             .concat("-")
             .concat(nftId.toString());
-    }
-
-    export function getOrCreateAirToken(chainID: string, address: string): AirToken {
-        let entity = AirToken.load(chainID + "-" + address);
-        if (entity == null) {
-            entity = new AirToken(chainID + "-" + address);
-            entity.address = address;
-            entity.save();
-        }
-        return entity as AirToken;
     }
 
     export function getOrCreateAirNftTransaction(
