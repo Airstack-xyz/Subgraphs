198868bf21dd3586a48e0beb35ee3fd138c27997
Rahul Gupta <rahul7668gupta@gmail.com>
2023-02-25 03:00:07 +0530

    rarible v1 and seaport wip fix

--- rarible-exchange-v1/src/exchange-v-1.ts
+++ rarible-exchange-v1/src/exchange-v-1.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts";
+import { BigInt, log } from "@graphprotocol/graph-ts";
 import { ExchangeCall } from "../generated/ExchangeV1/ExchangeV1";
 import * as airstack from "../modules/airstack/nft-marketplace";
 import * as utils from "./utils";
@@ -9,6 +9,9 @@ export function handleExchange(call: ExchangeCall): void {
   let buyAsset = call.inputs.order.key.buyAsset;
   if (sellAsset.assetType > 1) {
     // sellAsset is NFT,the owner has it & wants buy Asset
+    log.debug("sell asset address {} tokenId {} type {} hash {} sellAsset is NFT", [sellAsset.token.toHexString(), sellAsset.tokenId.toString(), sellAsset.assetType.toString(), call.transaction.hash.toHexString()])
+    log.debug("buy asset address {} tokenId {} type {} hash {} sellAsset is NFT", [buyAsset.token.toHexString(), buyAsset.tokenId.toString(), buyAsset.assetType.toString(), call.transaction.hash.toHexString()])
+    log.debug("call inputs hash {} amount {} buyerFee {} sellerFee {} buyer {} orderSelling {} seller {} buyAsset is NFT", [call.transaction.hash.toHexString(), call.inputs.amount.toString(), call.inputs.buyerFee.toString(), call.inputs.order.sellerFee.toString(), call.inputs.buyer.toString(), call.inputs.order.selling.toString(), call.inputs.order.key.owner.toHexString()])
     let paying = call.inputs.order.buying
       .times(call.inputs.amount)
       .div(call.inputs.order.selling);
@@ -64,7 +67,9 @@ export function handleExchange(call: ExchangeCall): void {
       .times(call.inputs.order.sellerFee)
       .div(BigInt.fromI32(10000));
     let paymentAmount = call.inputs.amount.plus(fee);
-
+    log.debug("sell asset address {} tokenId {} type {} hash {} buyAsset is NFT", [sellAsset.token.toHexString(), sellAsset.tokenId.toString(), sellAsset.assetType.toString(), call.transaction.hash.toHexString()])
+    log.debug("buy asset address {} tokenId {} type {} hash {} buyAsset is NFT", [buyAsset.token.toHexString(), buyAsset.tokenId.toString(), buyAsset.assetType.toString(), call.transaction.hash.toHexString()])
+    log.debug("call inputs hash {} amount {} buyerFee {} sellerFee {} buyer {} seller {} buyAsset is NFT", [call.transaction.hash.toHexString(), call.inputs.order.buying.toString(), call.inputs.buyerFee.toString(), call.inputs.order.sellerFee.toString(), call.inputs.order.key.owner.toHexString(), call.from.toHexString()])
     let beneficiaryDetails = utils.getFeeBeneficiaryDetails(
       call.inputs.amount,
       call.inputs.buyerFee,
@@ -82,7 +87,7 @@ export function handleExchange(call: ExchangeCall): void {
       buyAsset.token,
       buyAsset.assetType == 2 ? "ERC1155" : sellAsset.assetType == 3 ? "ERC721" : "UNKNOWN",
       buyAsset.tokenId,
-      call.inputs.amount,
+      call.inputs.order.buying,
     )
 
     let nftSales = new airstack.nft.Sale(
