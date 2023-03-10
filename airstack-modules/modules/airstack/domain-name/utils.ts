0a5cf6ed79150b4ffbe7b64d98283079b719fbe6
Rahul Gupta <rahul7668gupta@gmail.com>
2023-03-10 17:46:54 +0530

    moved air extra to domain name utils

--- airstack-modules/modules/airstack/domain-name/utils.ts
+++ airstack-modules/modules/airstack/domain-name/utils.ts
@@ -1,3 +1,7 @@
+import {
+  AirExtra,
+} from "../../../generated/schema";
+
 export const AIR_DOMAIN_OWNER_CHANGED_ENTITY_COUNTER_ID = "AIR_DOMAIN_OWNER_CHANGED_ENTITY_COUNTER";
 export const AIR_DOMAIN_TRANSFER_ENTITY_COUNTER_ID = "AIR_DOMAIN_TRANSFER_ENTITY_COUNTER";
 export const AIR_DOMAIN_NEW_RESOLVER_ENTITY_COUNTER_ID = "AIR_DOMAIN_NEW_RESOLVER_ENTITY_COUNTER";
@@ -13,3 +17,25 @@ export const AIR_EXTRA_TTL = 'ttl';
 
 export const ROOT_NODE = '0x0000000000000000000000000000000000000000000000000000000000000000'
 export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+
+/**
+ * @dev this function does not save the returned entity
+ * @dev this function creates an air extra entity
+ * @param name air extra name
+ * @param value air extra value
+ * @param extraId air extra entity id
+ * @returns air extra entity
+ */
+export function createAirExtra(
+  name: string,
+  value: string,
+  id: string,
+): AirExtra {
+  let entity = AirExtra.load(id);
+  if (entity == null) {
+    entity = new AirExtra(id);
+    entity.name = name;
+    entity.value = value;
+  }
+  return entity as AirExtra;
+}
