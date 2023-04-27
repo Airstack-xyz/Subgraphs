9460ca1a30a9460de454ef46c39f9fc83d2f2a92
Sharath V <sharathkrml@gmail.com>
2023-04-27 17:04:37 +0530

    added more test cases

--- lens/tests/lens.test.ts
+++ lens/tests/lens.test.ts
@@ -1,7 +1,17 @@
 import { Bytes, log } from "@graphprotocol/graph-ts"
 import { assert, beforeEach, clearStore, describe, test } from "matchstick-as/assembly/index"
 import { getDefaultProfileSet, getProfileCreatedEvent, getTransferEvent } from "./utils"
-import { createProfile1, createProfile2, profileCreated1, setDefault2, transfer3 } from "./sample"
+import {
+    createProfile1,
+    createProfile2,
+    minting,
+    profileCreated1,
+    setDefault2,
+    settingDefault,
+    transfer3,
+    transferring,
+    transferringAgain,
+} from "./sample"
 import { handleDefaultProfileSet, handleProfileCreated, handleTransfer } from "../src/mappings"
 import { LENSHUB_ADDRESS } from "../src/constants"
 
@@ -12,168 +22,245 @@ describe("Testing Lens Subgraph", () => {
     beforeEach(() => {
         clearStore() // <-- clear the store before each test in the file
     })
-    test("testing handleProfileCreated", () => {
-        let createProfile1Event = getProfileCreatedEvent(createProfile1)
-        handleProfileCreated(createProfile1Event)
-        let createProfile2Event = getProfileCreatedEvent(createProfile2)
-        handleProfileCreated(createProfile2Event)
-        assert.entityCount("UserToProfileMap", 1)
-        assert.fieldEquals("UserToProfileMap", createProfile1.to, "user", createProfile2.to)
-        assert.fieldEquals(
-            "UserToProfileMap",
-            createProfile1.to,
-            "profile",
-            `[${createProfile1.profileId}, ${createProfile2.profileId}]`
-        )
-        assert.fieldEquals(
-            "UserToProfileMap",
-            createProfile1.to,
-            "lensNames",
-            `[${createProfile1.handle}, ${createProfile2.handle}]`
-        )
-        assert.fieldEquals("UserToProfileMap", createProfile1.to, "count", "2")
-        assert.fieldEquals(
-            "UserToProfileMap",
-            createProfile1.to,
-            "hashes",
-            `[${createProfile1.txHash}, ${createProfile2.txHash}]`
-        )
-        // checking AirSocialUser
-        assert.entityCount("AirSocialUser", 1)
+    // test("testing handleProfileCreated", () => {
+    //     let createProfile1Event = getProfileCreatedEvent(createProfile1)
+    //     handleProfileCreated(createProfile1Event)
+    //     let createProfile2Event = getProfileCreatedEvent(createProfile2)
+    //     handleProfileCreated(createProfile2Event)
+    //     assert.entityCount("UserToProfileMap", 1)
+    //     assert.fieldEquals("UserToProfileMap", createProfile1.to, "user", createProfile2.to)
+    //     assert.fieldEquals(
+    //         "UserToProfileMap",
+    //         createProfile1.to,
+    //         "profile",
+    //         `[${createProfile1.profileId}, ${createProfile2.profileId}]`
+    //     )
+    //     assert.fieldEquals(
+    //         "UserToProfileMap",
+    //         createProfile1.to,
+    //         "lensNames",
+    //         `[${createProfile1.handle}, ${createProfile2.handle}]`
+    //     )
+    //     assert.fieldEquals("UserToProfileMap", createProfile1.to, "count", "2")
+    //     assert.fieldEquals(
+    //         "UserToProfileMap",
+    //         createProfile1.to,
+    //         "hashes",
+    //         `[${createProfile1.txHash}, ${createProfile2.txHash}]`
+    //     )
+    //     // checking AirSocialUser
+    //     assert.entityCount("AirSocialUser", 1)
+    //     assert.fieldEquals(
+    //         "AirSocialUser",
+    //         chainIdPrefix + createProfile1.to,
+    //         "address",
+    //         chainIdPrefix + createProfile1.to
+    //     )
+    //     // checking AirSocialProfile
+    //     assert.entityCount("AirSocialProfile", 2)
+    //     let profile1id =
+    //         chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + createProfile1.profileId
+    //     let profile2id =
+    //         chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + createProfile2.profileId
+    //     assert.fieldEquals("AirSocialProfile", profile1id, "name", createProfile1.handle)
+    //     assert.fieldEquals("AirSocialProfile", profile2id, "name", createProfile2.handle)
+    //     assert.fieldEquals(
+    //         "AirSocialProfile",
+    //         profile1id,
+    //         "user",
+    //         chainIdPrefix + createProfile1.to
+    //     )
+    //     assert.fieldEquals(
+    //         "AirSocialProfile",
+    //         profile2id,
+    //         "user",
+    //         chainIdPrefix + createProfile2.to
+    //     )
+    //     assert.fieldEquals("AirSocialProfile", profile1id, "tokenId", createProfile1.profileId)
+    //     assert.fieldEquals("AirSocialProfile", profile2id, "tokenId", createProfile2.profileId)
+    //     assert.fieldEquals(
+    //         "AirSocialProfile",
+    //         profile1id,
+    //         "tokenAddress",
+    //         chainIdPrefix + LENSHUB_ADDRESS.toHexString()
+    //     )
+    //     assert.fieldEquals(
+    //         "AirSocialProfile",
+    //         profile2id,
+    //         "tokenAddress",
+    //         chainIdPrefix + LENSHUB_ADDRESS.toHexString()
+    //     )
+    // })
+    // test("Testing minting,setting default profile,transferring & automatic unsetting", () => {
+    //     let profileCreatedEvent = getProfileCreatedEvent(profileCreated1)
+    //     handleProfileCreated(profileCreatedEvent)
+    //     // checking AirSocialUser
+    //     assert.entityCount("AirSocialUser", 1)
+    //     assert.fieldEquals(
+    //         "AirSocialUser",
+    //         chainIdPrefix + profileCreated1.to,
+    //         "address",
+    //         chainIdPrefix + profileCreated1.to
+    //     )
+    //     // checking AirSocialProfile
+    //     assert.entityCount("AirSocialProfile", 1)
+    //     let profile1id =
+    //         chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + profileCreated1.profileId
+    //     assert.fieldEquals("AirSocialProfile", profile1id, "name", profileCreated1.handle)
+    //     assert.fieldEquals(
+    //         "AirSocialProfile",
+    //         profile1id,
+    //         "user",
+    //         chainIdPrefix + profileCreated1.to
+    //     )
+    //     assert.fieldEquals("AirSocialProfile", profile1id, "tokenId", profileCreated1.profileId)
+    //     assert.fieldEquals(
+    //         "AirSocialProfile",
+    //         profile1id,
+    //         "tokenAddress",
+    //         chainIdPrefix + LENSHUB_ADDRESS.toHexString()
+    //     )
+    //     assert.fieldEquals("AirSocialProfile", profile1id, "isDefault", "false")
+    //     assert.fieldEquals(
+    //         "AirSocialProfile",
+    //         profile1id,
+    //         "user",
+    //         chainIdPrefix + profileCreated1.to
+    //     )
+
+    //     let defaultProfileSetEvent = getDefaultProfileSet(setDefault2)
+    //     handleDefaultProfileSet(defaultProfileSetEvent)
+    //     // checking DefaultProfileSetEntity
+    //     let defaultProfileSetId = setDefault2.txHash
+    //         .concat("-")
+    //         .concat(defaultProfileSetEvent.logIndex.toString())
+    //     assert.entityCount("DefaultProfileSet", 1)
+    //     assert.fieldEquals("DefaultProfileSet", defaultProfileSetId, "wallet", setDefault2.wallet)
+    //     assert.fieldEquals("DefaultProfileSet", defaultProfileSetId, "hash", setDefault2.txHash)
+    //     assert.fieldEquals(
+    //         "DefaultProfileSet",
+    //         defaultProfileSetId,
+    //         "profileId",
+    //         setDefault2.profileId
+    //     )
+    //     // checking UserToDefaultProfileIdMap
+    //     assert.entityCount("UserToDefaultProfileIdMap", 1)
+    //     assert.fieldEquals(
+    //         "UserToDefaultProfileIdMap",
+    //         setDefault2.wallet,
+    //         "profileId",
+    //         setDefault2.profileId
+    //     )
+    //     // checking defaultProfile in AirSocialUser
+    //     assert.fieldEquals(
+    //         "AirSocialUser",
+    //         chainIdPrefix + profileCreated1.to,
+    //         "defaultProfile",
+    //         chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + setDefault2.profileId
+    //     )
+    //     // checking isDefault in AirSocialProfile
+    //     assert.fieldEquals("AirSocialProfile", profile1id, "isDefault", "true")
+    //     // checking AirSocialUserDefaultProfileChangeTransaction
+    //     assert.fieldEquals(
+    //         "AirSocialUserDefaultProfileChangeTransaction",
+    //         setDefault2.txHash + joiner + chainIdPrefix + chainIdPrefix + profileCreated1.to,
+    //         "oldDefaultProfile",
+    //         "null"
+    //     )
+    //     assert.fieldEquals(
+    //         "AirSocialUserDefaultProfileChangeTransaction",
+    //         setDefault2.txHash + joiner + chainIdPrefix + chainIdPrefix + profileCreated1.to,
+    //         "newDefaultProfile",
+    //         chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + setDefault2.profileId
+    //     )
+    //     let transferEvent = getTransferEvent(transfer3)
+    //     handleTransfer(transferEvent)
+    //     // checking defaultProfile in AirSocialUser (cleared to null)
+    //     assert.fieldEquals(
+    //         "AirSocialUser",
+    //         chainIdPrefix + profileCreated1.to,
+    //         "defaultProfile",
+    //         "null"
+    //     )
+    //     // checking isDefault in AirSocialProfile
+    //     assert.fieldEquals("AirSocialProfile", profile1id, "isDefault", "false")
+    //     assert.fieldEquals("AirSocialProfile", profile1id, "user", chainIdPrefix + transfer3.to)
+    //     // checking AirSocialUserDefaultProfileChangeTransaction
+    //     assert.fieldEquals(
+    //         "AirSocialUserDefaultProfileChangeTransaction",
+    //         transfer3.txHash + joiner + chainIdPrefix + chainIdPrefix + profileCreated1.to,
+    //         "oldDefaultProfile",
+    //         chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + setDefault2.profileId
+    //     )
+    //     assert.fieldEquals(
+    //         "AirSocialUserDefaultProfileChangeTransaction",
+    //         transfer3.txHash + joiner + chainIdPrefix + chainIdPrefix + profileCreated1.to,
+    //         "newDefaultProfile",
+    //         "null"
+    //     )
+    // })
+    test("Testing minting transferring ,setting default & transferringAgain", () => {
+        let mintingEvent = getProfileCreatedEvent(minting)
+        let profileId = chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + minting.profileId
+        handleProfileCreated(mintingEvent)
+        let mintedUserId = chainIdPrefix + minting.to
+        // -- profiles of minted user
         assert.fieldEquals(
             "AirSocialUser",
-            chainIdPrefix + createProfile1.to,
-            "address",
-            chainIdPrefix + createProfile1.to
+            mintedUserId,
+            "profiles",
+            `[${chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + minting.profileId}]`
         )
-        // checking AirSocialProfile
-        assert.entityCount("AirSocialProfile", 2)
-        let profile1id =
-            chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + createProfile1.profileId
-        let profile2id =
-            chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + createProfile2.profileId
-        assert.fieldEquals("AirSocialProfile", profile1id, "name", createProfile1.handle)
-        assert.fieldEquals("AirSocialProfile", profile2id, "name", createProfile2.handle)
-        assert.fieldEquals(
-            "AirSocialProfile",
-            profile1id,
-            "user",
-            chainIdPrefix + createProfile1.to
-        )
-        assert.fieldEquals(
-            "AirSocialProfile",
-            profile2id,
-            "user",
-            chainIdPrefix + createProfile2.to
-        )
-        assert.fieldEquals("AirSocialProfile", profile1id, "tokenId", createProfile1.profileId)
-        assert.fieldEquals("AirSocialProfile", profile2id, "tokenId", createProfile2.profileId)
-        assert.fieldEquals(
-            "AirSocialProfile",
-            profile1id,
-            "tokenAddress",
-            chainIdPrefix + LENSHUB_ADDRESS.toHexString()
-        )
-        assert.fieldEquals(
-            "AirSocialProfile",
-            profile2id,
-            "tokenAddress",
-            chainIdPrefix + LENSHUB_ADDRESS.toHexString()
-        )
-    })
-    test("Testing minting,setting default profile,transferring & automatic unsetting", () => {
-        let profileCreatedEvent = getProfileCreatedEvent(profileCreated1)
-        handleProfileCreated(profileCreatedEvent)
-        // checking AirSocialUser
-        assert.entityCount("AirSocialUser", 1)
+        // --- profile owner
+        assert.fieldEquals("AirSocialProfile", profileId, "user", chainIdPrefix + minting.to)
+
+        // transferring
+        let transferringEvent = getTransferEvent(transferring)
+        handleTransfer(transferringEvent)
+
+        // --- profile owner
+        assert.fieldEquals("AirSocialProfile", profileId, "user", chainIdPrefix + transferring.to)
+        // --- profile default status
+        assert.fieldEquals("AirSocialProfile", profileId, "isDefault", "false")
+
+        // --- profiles of minted user
+        assert.fieldEquals("AirSocialUser", mintedUserId, "profiles", `[]`)
+
+        // --- profiles of new user
+        let transferredToUserId = chainIdPrefix + transferring.to
+        assert.fieldEquals("AirSocialUser", transferredToUserId, "id", transferredToUserId)
         assert.fieldEquals(
             "AirSocialUser",
-            chainIdPrefix + profileCreated1.to,
-            "address",
-            chainIdPrefix + profileCreated1.to
+            transferredToUserId,
+            "profiles",
+            `[${chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + transferring.tokenId}]`
         )
-        // checking AirSocialProfile
-        assert.entityCount("AirSocialProfile", 1)
-        let profile1id =
-            chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + profileCreated1.profileId
-        assert.fieldEquals("AirSocialProfile", profile1id, "name", profileCreated1.handle)
-        assert.fieldEquals(
-            "AirSocialProfile",
-            profile1id,
-            "user",
-            chainIdPrefix + profileCreated1.to
-        )
-        assert.fieldEquals("AirSocialProfile", profile1id, "tokenId", profileCreated1.profileId)
-        assert.fieldEquals(
-            "AirSocialProfile",
-            profile1id,
-            "tokenAddress",
-            chainIdPrefix + LENSHUB_ADDRESS.toHexString()
-        )
-        let defaultProfileSetEvent = getDefaultProfileSet(setDefault2)
-        handleDefaultProfileSet(defaultProfileSetEvent)
-        // checking DefaultProfileSetEntity
-        let defaultProfileSetId = setDefault2.txHash
-            .concat("-")
-            .concat(defaultProfileSetEvent.logIndex.toString())
-        assert.entityCount("DefaultProfileSet", 1)
-        assert.fieldEquals("DefaultProfileSet", defaultProfileSetId, "wallet", setDefault2.wallet)
-        assert.fieldEquals("DefaultProfileSet", defaultProfileSetId, "hash", setDefault2.txHash)
-        assert.fieldEquals(
-            "DefaultProfileSet",
-            defaultProfileSetId,
-            "profileId",
-            setDefault2.profileId
-        )
-        // checking UserToDefaultProfileIdMap
-        assert.entityCount("UserToDefaultProfileIdMap", 1)
-        assert.fieldEquals(
-            "UserToDefaultProfileIdMap",
-            setDefault2.wallet,
-            "profileId",
-            setDefault2.profileId
-        )
-        // checking defaultProfile in AirSocialUser
+        // --- checking default profile of user before setting
+        // setting default
+        let setDetaultEvent = getDefaultProfileSet(settingDefault)
+        handleDefaultProfileSet(setDetaultEvent)
+        // -- check current user's defaultProfile
         assert.fieldEquals(
             "AirSocialUser",
-            chainIdPrefix + profileCreated1.to,
+            transferredToUserId,
             "defaultProfile",
-            chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + setDefault2.profileId
+            chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + minting.profileId
         )
-        // checking AirSocialUserDefaultProfileChangeTransaction
-        assert.fieldEquals(
-            "AirSocialUserDefaultProfileChangeTransaction",
-            setDefault2.txHash + joiner + chainIdPrefix + chainIdPrefix + profileCreated1.to,
-            "oldDefaultProfile",
-            "null"
-        )
-        assert.fieldEquals(
-            "AirSocialUserDefaultProfileChangeTransaction",
-            setDefault2.txHash + joiner + chainIdPrefix + chainIdPrefix + profileCreated1.to,
-            "newDefaultProfile",
-            chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + setDefault2.profileId
-        )
-        let transferEvent = getTransferEvent(transfer3)
-        handleTransfer(transferEvent)
-        // checking defaultProfile in AirSocialUser (cleared to null)
-        assert.fieldEquals(
-            "AirSocialUser",
-            chainIdPrefix + profileCreated1.to,
-            "defaultProfile",
-            "null"
-        )
-        // checking AirSocialUserDefaultProfileChangeTransaction
-        assert.fieldEquals(
-            "AirSocialUserDefaultProfileChangeTransaction",
-            transfer3.txHash + joiner + chainIdPrefix + chainIdPrefix + profileCreated1.to,
-            "oldDefaultProfile",
-            chainIdPrefix + LENSHUB_ADDRESS.toHexString() + joiner + setDefault2.profileId
-        )
-        assert.fieldEquals(
-            "AirSocialUserDefaultProfileChangeTransaction",
-            transfer3.txHash + joiner + chainIdPrefix + chainIdPrefix + profileCreated1.to,
-            "newDefaultProfile",
-            "null"
-        )
+        //  -- checking default status
+        assert.fieldEquals("AirSocialProfile", profileId, "isDefault", "true")
+
+        // transferring Again
+        let transferAgainEvent = getTransferEvent(transferringAgain)
+        handleTransfer(transferAgainEvent)
+        let transferringAgainUserId = chainIdPrefix + transferringAgain.to
+        //  -- checking default status
+        assert.fieldEquals("AirSocialProfile", profileId, "isDefault", "false")
+        //  -- checking current user
+        assert.fieldEquals("AirSocialProfile", profileId, "user", transferringAgainUserId)
+        // -- checking profiles of current user
+        assert.fieldEquals("AirSocialUser", transferringAgainUserId, "profiles", `[${profileId}]`)
+
+        // -- checking profiles of old user
+        assert.fieldEquals("AirSocialUser", transferredToUserId, "profiles", `[]`)
     })
 })
--- lens/tests/sample.ts
+++ lens/tests/sample.ts
@@ -25,7 +25,6 @@ export const createProfile2: ProfileCreatedInput = {
     timestamp: "1661954426",
 }
 
-
 // 28471068 - profile gets created
 export const profileCreated1: ProfileCreatedInput = {
     txHash: "0x2b75f630d40acd75fbeabb7ca5eba7afb7421f75faee2522751072d620aad991",
@@ -52,4 +51,63 @@ export const transfer3: TransferInput = {
     from: "0x34b1987af70a43a0647860bdda720ba3664740e8",
     to: "0xa0592643b2f033247b56aba944d362dd0d0e6f66",
     tokenId: "882",
+}
+// //
+// {
+//   "data": {
+//     "transferEntities": [
+//       {
+//         "fromTo": [
+//           "0x0000000000000000000000000000000000000000-0x8b4c757039677b382d2f4b90c4c9695c20b5491a",
+//           "0x8b4c757039677b382d2f4b90c4c9695c20b5491a-0x022824cdbe3854f922eedeb8c925fb2a8182b78c",
+//           "0x022824cdbe3854f922eedeb8c925fb2a8182b78c-0x894d1fce0d11afed2480ddf8985ce641a1201d29"
+//         ],
+//         "count": "3",
+//         "hashes": [
+//           "0xc9d99aace4a0861004bce3123c850d66248e5a0ac3c6c507a196411e5facd909",
+//           "0x33b3bf37e5ec92726de84ee00a303498290e65c369f19e2053eacd2dbda2910e",
+//           "0xfc6f9a053f58e28828e8f2c811e05f3890da7b7547deb3e4f562496655bd489d"
+//         ],
+//         "tokenId": "4588"
+//       }
+//     ],
+//     "defaultProfileSets": [
+//       {
+//         "hash": "0x0f8a5d79c52bf18fef26d4a88f13a9c21792fdeebd2cbea3fbf59ee66b34f2ab",
+//         "wallet": "0x022824cdbe3854f922eedeb8c925fb2a8182b78c",
+//         "profileId": "4588"
+//       }
+//     ]
+//   }
+// }
+
+export const minting: ProfileCreatedInput = {
+    txHash: "0xc9d99aace4a0861004bce3123c850d66248e5a0ac3c6c507a196411e5facd909",
+    profileId: "4588",
+    creator: "0x1eec6eccaa4625da3fa6cd6339dbcc2418710e8a",
+    to: "0x8b4c757039677b382d2f4b90c4c9695c20b5491a",
+    handle: "danielbrine111.lens",
+    imageURI: "",
+    followModule: "0x0000000000000000000000000000000000000000",
+    followModuleReturnData: "0x",
+    followNFTURI: "ipfs://QmRairzvvpjR39W7PHmoUBKTzpW46BfhMKKSXG4NLMiNcW",
+    timestamp: "1652923149",
+}
+export const transferring: TransferInput = {
+    txHash: "0x33b3bf37e5ec92726de84ee00a303498290e65c369f19e2053eacd2dbda2910e",
+    from: "0x8b4c757039677b382d2f4b90c4c9695c20b5491a",
+    to: "0x022824cdbe3854f922eedeb8c925fb2a8182b78c",
+    tokenId: "4588",
+}
+export const settingDefault: DefaultProfileSetInput = {
+    txHash: "0x0f8a5d79c52bf18fef26d4a88f13a9c21792fdeebd2cbea3fbf59ee66b34f2ab",
+    wallet: "0x022824cdbe3854f922eedeb8c925fb2a8182b78c",
+    profileId: "4588",
+    timestamp: "1652934849",
+}
+export const transferringAgain: TransferInput = {
+    txHash: "0xfc6f9a053f58e28828e8f2c811e05f3890da7b7547deb3e4f562496655bd489d",
+    from: "0x022824cdbe3854f922eedeb8c925fb2a8182b78c",
+    to: "0x894d1fce0d11afed2480ddf8985ce641a1201d29",
+    tokenId: "4588",
 }
