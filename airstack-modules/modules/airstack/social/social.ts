7292007df1bbd5a5b2845e10667b8818a31a3950
0xsarvesh <sarvesh.codeworks@gmail.com>
2023-12-01 19:54:31 +0530

    Fixed lensv2 subgraph

--- airstack-modules/modules/airstack/social/social.ts
+++ airstack-modules/modules/airstack/social/social.ts
@@ -887,25 +887,30 @@ export function trackSocialProfileHandleTransferTransaction(
 
   entity.owner = airAccount.id;  
 
-  const profile = AirSocialProfile.load(entity.profile!);
-  // if handle is burned, unlink it with profile if already linked
-  if(to == ZERO_ADDRESS && entity.profile != null) {
+  if (entity.profile) {
 
-    if(profile != null) {
-      trackSocialProfileHandleUnlinkTransaction(
-        block,
-        tokenAddress,
-        tokenId,
-        profile.tokenAddress,
-        tokenId
-      )
+    const profile = AirSocialProfile.load(entity.profile!);
+    // if handle is burned, unlink it with profile if already linked
+    if (to == ZERO_ADDRESS && entity.profile != null) {
+
+      if (profile != null) {
+        trackSocialProfileHandleUnlinkTransaction(
+          block,
+          tokenAddress,
+          tokenId,
+          profile.tokenAddress,
+          tokenId
+        )
+      }
     }
+
+    if (profile != null) {
+      saveAirSocialProfile(profile, airBlock);
+    }
+
   }
   saveProfileHandle(entity, airBlock)
-  if(profile != null) {
-    saveAirSocialProfile(profile, airBlock);
-  }
-  
+
 }
 
 
