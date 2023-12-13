ab553559447ef02421d52b2069c1401ba16a2b15
Sharath V <sharathkrml@gmail.com>
2023-12-13 16:35:36 +0530

    added test

--- ens-v2/tests/reverse-registrar.test.ts
+++ ens-v2/tests/reverse-registrar.test.ts
@@ -1,4 +1,4 @@
-import { Address, BigInt, Bytes, log } from "@graphprotocol/graph-ts"
+import { Address, BigInt, Bytes, ethereum, log } from "@graphprotocol/graph-ts"
 import {
   assert,
   beforeEach,
@@ -6,29 +6,11 @@ import {
   describe,
   test,
 } from "matchstick-as/assembly/index"
+import * as airstack from "../modules/airstack/domain-name"
 
-import {
-  mockHandleNewOwner,
-  mockHandleNewResolver,
-  mockHandleTransfer,
-} from "./ens-registry-utils"
-import {
-  intialTransfer,
-  newResolver,
-  rootNewOwner,
-  childNewOwner,
-  childNewResolver,
-} from "./ens-old-example"
-import { getAddrChangedEvent } from "./resolver-utils"
-import { resolvedAddressSet, resolvedAddressSetChild } from "./resolver-example"
-import { handleAddrChanged } from "../src/resolver"
-import { HandleSetNameInput, mockSetName } from "./reverse-registrar-utils"
-import {
-  primaryDomainSet,
-  primaryDomainSet2,
-} from "./reverse-registrar-example"
 import { AirDomain, AirResolver } from "../generated/schema"
 import { mockAirBlock, mockAirDomainAccount, mockAirLabelName } from "./mock"
+import { newBlock } from "./utils"
 export const chainIdPrefix = "1-"
 export const joiner = "-"
 
@@ -38,28 +20,31 @@ describe("Testing Reverse registrar", () => {
   })
   test("testing primary set change", () => {
     let airBlock = mockAirBlock()
-
+    airBlock.save()
     let airLabelName1 = mockAirLabelName("0xeth", "eth")
-    let airLabelName2 = mockAirLabelName("0xa", "a")
-
+    airLabelName1.save()
+    let airLabelName2 = mockAirLabelName("0xmozilla", "mozilla")
+    airLabelName2.save()
     let airDomainAccount = mockAirDomainAccount("0xuserAddress")
+    airDomainAccount.save()
 
     // creating airDomain
     const domainId =
-      "0x83853f8c4ca91ae85231d68dec421e7d9210f65860b863a574dfc0dc0c7e815e"
+      "0x0028ddb6c1ffcc054599b0b53a0510825538a6f16d1a0db09e7cbbdf7084f090"
     let airDomain = new AirDomain(domainId)
-    airDomain.encodedName = "0xa.0xeth"
+
+    airDomain.encodedName = "0xmozilla.0xeth"
     airDomain.name = [airLabelName1.id, airLabelName2.id]
     airDomain.labelName = airLabelName2.id
     airDomain.subdomainCount = BigInt.fromI32(0)
     airDomain.fuses = BigInt.fromI32(0)
     airDomain.manager = airDomainAccount.id
     airDomain.owner = airDomainAccount.id
-    airDomain.isPrimary = false
     airDomain.isNameWrapped = false
     airDomain.createdAt = airBlock.id
     airDomain.lastUpdatedIndex = BigInt.fromI32(0)
     airDomain.save()
+    assert.fieldEquals("AirDomain", domainId, "lastUpdatedIndex", "1")
 
     // creating airResolver
     let resolverAddress = "0x9062C0A6Dbd6108336BcBe4593a3D1cE05512069".toLowerCase()
@@ -78,14 +63,16 @@ describe("Testing Reverse registrar", () => {
     // setting resolver
     airDomain.resolver = airResolver.id
     airDomain.save()
-    let setNameInput: HandleSetNameInput = {
-      name: "a",
-      hash:
-        "0xe6ac67ca8da45ca7522f66f184d9912360e3498274eea6c15021b2658d90d4d0",
-      callIndex: "1",
-      from: resolvedAddress,
-    }
-    mockSetName(setNameInput)
+    airstack.domain.trackSetName(
+      Bytes.fromHexString(
+        "0xe6ac67ca8da45ca7522f66f184d9912360e3498274eea6c15021b2658d90d4d0"
+      ),
+      BigInt.fromString("1"),
+      "mozilla.eth",
+      "0x0028ddb6c1ffcc054599b0b53a0510825538a6f16d1a0db09e7cbbdf7084f090",
+      Address.fromString(resolvedAddress),
+      newBlock()
+    )
     assert.fieldEquals("AirDomain", domainId, "isPrimary", "true")
     assert.fieldEquals("AirDomain", domainId, "lastUpdatedIndex", "1")
   })
