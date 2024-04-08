06c02ec574d14a8c2e04e587a4a5b0dc482464e9
Hrishikesh Thakkar <kesh5210@gmail.com>
2024-04-08 20:39:08 +0400

    Updating to Gold Chain

--- cropxyz/src/constants.ts
+++ cropxyz/src/constants.ts
@@ -1,3 +1,3 @@
 import { Address } from "@graphprotocol/graph-ts"
 // address for Plot entity
-export const PLOT_NFT_ADDRESS = Address.fromHexString("0x389e11255a78db6c8eddf8b5e112470f026d19e2")
+export const PLOT_NFT_ADDRESS = Address.fromHexString("0xF3b28069ff9560ECAf645635792586c4821e9348")
